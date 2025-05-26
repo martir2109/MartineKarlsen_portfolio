3d1a70bb76f2008c8171558c4e8b60190a73f703
Martine R Karlsen <martinekar08@gmail.com>
2025-05-26 17:51:24 +0200

    I have begun working on my portfolio website. only in the beginning stage

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MartineKarlsen_portfolio
+
+Hello! Welcome to my repository for my Portfolio. This portfolio include projects I have completed in my UX-design and Front-end development studies.
---
+++ js/navbar.js
@@ -0,0 +1,12 @@
+function toggleNavbar() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+function toggleIcon(x) {
+  x.classList.toggle("change");
+}
